5a943d5b1ea2d2f29ed6810a30f8ee4123dec087
Fras0 <frasasker0@gmail.com>
2024-10-26 17:51:45 +0300

    added cookies,fixed update password

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -6,4 +6,29 @@ const generateToken = (id) => {
   })
 }
 
-export { generateToken };
+const createSendToken = (user, statusCode, res) => {
+  const token = generateToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove the password from the output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+export { createSendToken };
